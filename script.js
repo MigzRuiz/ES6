a0f45f669789a343224d2950ef3dd6b16f9043f0
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-13 15:12:25 -0700

    Finished Lecture 4

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-//Lecture 3: Strings
-let firstName  = "John";
-let lastName = "Smith";
-const age = 23;
+//Lecture 4: Arrow Functions
+
+const years = [1992,1994,2012,2009,2007];
+
+//ES5
+var age5 = years.map(function(el){
+    return 2018 - el;
+});
+
+console.log(age5);
 
 //ES6
-//Now has template literals - you use backticks
-console.log(`Hi ${firstname} ${lastName}. Nice to meet you.`);
+let age6 = years.map(el => `Age: ${2018-el}` );
+console.log(age6);
 
-//There are a few methods that we can use as well
-const n = `${firstName} ${lastName}`;
-console.log(n.startsWith("j")); //false
-console.log(n.endsWith("th")); //true
-console.log(n.includes(" ")); //true - checks if the string has that character
-console.log(firstName.repeat(3)); //JohnJohnJohn
-console.log(`${firstName} `.repeat(3)); //John John John
+//Return is implicit
+age6 = years.map((el, index) => `Age ${index + 1}: ${2018-el}`);
+console.log(age6);
+
+age6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    age = now - el;
+
+    return `Age ${index + 1}: ${age}`;
+});
+console.log(age6);
