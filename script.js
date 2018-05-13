29fc2814b1903392d1a7bc9b1ece6a21fdb0711e
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-13 13:33:35 -0700

    Finished Lecture3

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-//Lecture 2: Blocks and IIFEs
-
-//ES5
-(function () {
-    var c = 3;
-})();
+//Lecture 3: Strings
+let firstName  = "John";
+let lastName = "Smith";
+const age = 23;
 
 //ES6
-{
-    const a = 1;
-    let b = 2;
-    var c = 3;
-}
+//Now has template literals - you use backticks
+console.log(`Hi ${firstname} ${lastName}. Nice to meet you.`);
 
-console.log(c);
+//There are a few methods that we can use as well
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith("j")); //false
+console.log(n.endsWith("th")); //true
+console.log(n.includes(" ")); //true - checks if the string has that character
+console.log(firstName.repeat(3)); //JohnJohnJohn
+console.log(`${firstName} `.repeat(3)); //John John John
