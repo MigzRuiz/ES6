1ef9b1857d66e768d43ec33b6460963ce674018c
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2018-05-14 20:34:39 -0700

    Finished Lecture 6

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-//Lecture 4: Arrow Functions
-
-const years = [1992,1994,2012,2009,2007];
+//Lecture 6: Destructuring
 
 //ES5
-var age5 = years.map(function(el){
-    return 2018 - el;
-});
-
-console.log(age5);
+var john = ["John",25];
+var name5 = john[0];
+var age5 = john[1];
 
 //ES6
-let age6 = years.map(el => `Age: ${2018-el}` );
-console.log(age6);
+const [name, age] = ["John", 25];
 
-//Return is implicit
-age6 = years.map((el, index) => `Age ${index + 1}: ${2018-el}`);
-console.log(age6);
+console.log(name);
+console.log(age);
 
-age6 = years.map((el, index) => {
-    const now = new Date().getFullYear();
-    age = now - el;
+//You can use destructuring on objects too
+const personObj = {
+    firstName: "Pedro",
+    lastName: "Sandisk"
+};
 
-    return `Age ${index + 1}: ${age}`;
-});
-console.log(age6);
+const { firstName, lastName } = personObj;
+
+console.log(firstName);
+console.log(lastName);
+
+const { firstName: a, lastName: b } = personObj;
+
+console.log(a);
+console.log(b);
+
+//Practical example using functions
+//Let's say we want to get the age and retirement years based on the year
+
+function calcRetirement(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
+}
+
+const [age2, retirement] = calcRetirement(1995);
+console.log(`Age is ${age2} and retires at ${retirement}`);
+
